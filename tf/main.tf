27ebcfb59eff56c68b26d906129a8f624434b657
Antonio Merino <antoniomerino.ing@gmail.com>
2024-09-27 00:35:14 +0200

    Implement variables

--- tf/main.tf
+++ tf/main.tf
@@ -22,11 +22,33 @@ resource "railway_service" "gymrats_image_server_service" {
   project_id = railway_project.gymrats_image_server_project.id
 }
 
-# resource "railway_variable" "bucket_name" {
-#   service_id = railway_service.flask_api.id
-#   name       = "BUCKET_NAME"
-#   value      = var.bucket_name
-# }
+resource "railway_variable" "storage_mode" {
+  name            = "STORAGE_MODE"
+  value           = var.storage_mode
+  environment_id  = railway_project.gymrats_image_server_project.default_environment.id
+  service_id      = railway_service.gymrats_image_server_service.id
+}
+
+resource "railway_variable" "b2_bucket_name" {
+  name            = "B2_BUCKET_NAME"
+  value           = var.b2_bucket_name
+  environment_id  = railway_project.gymrats_image_server_project.default_environment.id
+  service_id      = railway_service.gymrats_image_server_service.id
+}
+
+resource "railway_variable" "b2_key_id" {
+  name            = "B2_KEY_ID"
+  value           = var.b2_key_id
+  environment_id  = railway_project.gymrats_image_server_project.default_environment.id
+  service_id      = railway_service.gymrats_image_server_service.id
+}
+
+resource "railway_variable" "b2_application_key" {
+  name            = "B2_APPLICATION_KEY"
+  value           = var.b2_application_key
+  environment_id  = railway_project.gymrats_image_server_project.default_environment.id
+  service_id      = railway_service.gymrats_image_server_service.id
+}
 
 resource "railway_deployment_trigger" "gymrats_image_server_deployment_trigger" {
   repository     = var.github_repository
--- tf/variables.tf
+++ tf/variables.tf
@@ -12,3 +12,23 @@ variable "branch_name" {
   type        = string
   description = "Name of the current Git branch"
 }
+
+variable "storage_mode" {
+  type        = string
+  description = "Storage mode for Image Server"
+}
+
+variable "b2_bucket_name" {
+  type        = string
+  description = "Backblaze B2 Bucket name"
+}
+
+variable "b2_key_id" {
+  type        = string
+  description = "Backblaze B2 KeyID"
+}
+
+variable "b2_application_key" {
+  type        = string
+  description = "Backblaze B2 ApplicationKey"
+}

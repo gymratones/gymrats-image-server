60071dff28c87eca4d941fed9b94d540323df032
Antonio Merino <antoniomerino.ing@gmail.com>
2024-09-26 20:28:56 +0200

    Change default_environment

--- tf/main.tf
+++ tf/main.tf
@@ -18,10 +18,10 @@ resource "railway_project" "gymrats_image_server_project" {
   }
 }
 
-resource "railway_environment" "gymrats_image_server_environment" {
-  name       = "staging"
-  project_id = railway_project.gymrats_image_server_project.id
-}
+# resource "railway_environment" "gymrats_image_server_environment" {
+#   name       = "staging"
+#   project_id = railway_project.gymrats_image_server_project.id
+# }
 
 resource "railway_service" "gymrats_image_server_service" {
   name       = "gymrats.service.test"

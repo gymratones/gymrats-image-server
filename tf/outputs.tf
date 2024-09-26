ce433eb2d1349d76d86c08aad794522baee60b8e
Antonio Merino <antoniomerino.ing@gmail.com>
2024-09-26 20:03:58 +0200

    Remove

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -21,15 +21,3 @@ output "service_name" {
   value       = railway_service.gymrats_image_server_service.name
   description = "The name of the Railway service"
 }
-
-# Output the service URL
-output "service_url" {
-  value       = railway_service.gymrats_image_server_service.service_url
-  description = "The URL of the deployed Railway service"
-}
-
-# Output the environment ID (assuming you're using a specific environment)
-output "environment_id" {
-  value       = railway_service.gymrats_image_server_service.environment_id
-  description = "The ID of the Railway environment"
-}

e7934b1f0d0e7010ab522698e4ab7a718e2b6bfe
Antonio Merino <antoniomerino.ing@gmail.com>
2024-09-26 19:35:58 +0200

    Simplify

--- tf/main.tf
+++ tf/main.tf
@@ -10,8 +10,30 @@ provider "railway" {
   token = "0a9b9e96-87d7-4fa9-87ff-d60f49242363"
 }
 
-module "railway_deployment" {
-  source = "./modules/railway"
+# module "railway_deployment" {
+#   source = "./modules/railway"
   
-  bucket_name = var.BUCKET_NAME
-}
+#   bucket_name = var.BUCKET_NAME
+# }
+
+# resource "railway_project" "flask_api" {
+#   name = "gymrats-terraform-test-project"
+# }
+
+# resource "railway_service" "flask_api" {
+#   project_id = railway_project.flask_api.id
+#   name       = "gymrats-terraform-test-api-service"
+# }
+
+# resource "railway_variable" "bucket_name" {
+#   service_id = railway_service.flask_api.id
+#   name       = "BUCKET_NAME"
+#   value      = var.bucket_name
+# }
+
+# resource "railway_deployment" "flask_api" {
+#   service_id = railway_service.flask_api.id
+#   triggers = {
+#     code = filesha256("${path.module}/../../../app.py")
+#   }
+# }
--- tf/modules/railway/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "service_url" {
-  value       = railway_service.flask_api.service_url
-  description = "URL of the service deployed on Railway"
-}

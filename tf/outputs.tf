0f68eeffe651308fe2f6cc0657150202175ba646
Antonio Merino <antoniomerino.ing@gmail.com>
2024-09-26 19:45:21 +0200

    Skip outputs

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,4 +1,4 @@
-output "service_url" {
-  value       = module.railway_deployment.service_url
-  description = "URL of the service deployed on Railway"
-}
+# output "service_url" {
+#   value       = module.railway_deployment.service_url
+#   description = "URL of the service deployed on Railway"
+# }

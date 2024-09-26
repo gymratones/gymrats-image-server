bc2c58562f53034a138cc47a005dc2bdc0009441
Antonio Merino <antoniomerino.ing@gmail.com>
2024-09-27 01:20:19 +0200

    Move to variable collection

--- tf/main.tf
+++ tf/main.tf
@@ -22,32 +22,28 @@ resource "railway_service" "gymrats_image_server_service" {
   project_id = railway_project.gymrats_image_server_project.id
 }
 
-resource "railway_variable" "storage_mode" {
-  name            = "STORAGE_MODE"
-  value           = var.storage_mode
-  environment_id  = railway_project.gymrats_image_server_project.default_environment.id
-  service_id      = railway_service.gymrats_image_server_service.id
-}
+resource "railway_variable_collection" "gymrats_image_server_variable_collection" {
+  environment_id = railway_project.gymrats_image_server_project.default_environment.id
+  service_id     = railway_service.gymrats_image_server_service.id
 
-resource "railway_variable" "b2_bucket_name" {
-  name            = "B2_BUCKET_NAME"
-  value           = var.b2_bucket_name
-  environment_id  = railway_project.gymrats_image_server_project.default_environment.id
-  service_id      = railway_service.gymrats_image_server_service.id
-}
-
-resource "railway_variable" "b2_key_id" {
-  name            = "B2_KEY_ID"
-  value           = var.b2_key_id
-  environment_id  = railway_project.gymrats_image_server_project.default_environment.id
-  service_id      = railway_service.gymrats_image_server_service.id
-}
-
-resource "railway_variable" "b2_application_key" {
-  name            = "B2_APPLICATION_KEY"
-  value           = var.b2_application_key
-  environment_id  = railway_project.gymrats_image_server_project.default_environment.id
-  service_id      = railway_service.gymrats_image_server_service.id
+  variables = [
+    {
+      name  = "STORAGE_MODE"
+      value = var.storage_mode
+    },
+    {
+      name  = "B2_BUCKET_NAME"
+      value = var.b2_bucket_name
+    },
+    {
+      name  = "B2_KEY_ID"
+      value = var.b2_key_id
+    },
+    {
+      name  = "B2_APPLICATION_KEY"
+      value = var.b2_application_key
+    }
+  ]
 }
 
 resource "railway_deployment_trigger" "gymrats_image_server_deployment_trigger" {

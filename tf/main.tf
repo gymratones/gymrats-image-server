e63bf1f977e4a6320710f7d112399f4b208d4205
Antonio Merino del Pozo <38297515+deelpoo@users.noreply.github.com>
2024-09-29 13:44:13 +0200

    Implement Dynamic Terraform Workspace creation (#19)
    * implement dynamic workspace creation
    
    * Cleanup
    
    * Restrict ci to staging

--- tf/main.tf
+++ tf/main.tf
@@ -1,12 +1,12 @@
 terraform {
-  cloud {
+  backend "remote" {
     organization = "gymrats"
     hostname     = "app.terraform.io"
     workspaces {
-      name    = "staging"
-      project = "gymrats-image-server"
+      prefix = "gr-"
     }
   }
+
   required_providers {
     render = {
       source  = "render-oss/render"
@@ -20,20 +20,8 @@ provider "render" {
   owner_id = var.provider_account_id
 }
 
-resource "render_project" "gymrats_image_server_project" {
-  name = "${local.project_name}-project"
-  environments = {
-    "${var.branch_name}" : {
-      name : var.branch_name,
-      protected_status : "protected"
-    }
-  }
-}
-
 resource "render_web_service" "gymrats_image_server_service" {
   name           = local.project_name
-  depends_on     = [render_project.gymrats_image_server_project]
-  environment_id = render_project.gymrats_image_server_project.environments[var.branch_name].id
   plan           = "starter"
   region         = "frankfurt"
   start_command  = "gunicorn app:app"
